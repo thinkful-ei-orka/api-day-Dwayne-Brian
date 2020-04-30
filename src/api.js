67d58e3e3009d815985fd9d4a5bd7a84e138511a
Victor Jarvis <vctrjrvs@gmail.com>
2020-04-30 13:51:01 -0400

    changes made to js

--- src/api.js
+++ src/api.js
@@ -1,11 +1,26 @@
 
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/ei-student/';
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/victor-brian/';
 
 function getItems() {
     return fetch(`${BASE_URL}items`)
     //return Promise.resolve('A successful response!');
 }
 
+function createItem(name) {
+    let newItem = {
+        name 
+    };
+
+    fetch(`${BASE_URL}items`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newItem)
+    });
+}
+
 export default {
-getItems
+getItems,
+createItem
 };
--- src/index.js
+++ src/index.js
@@ -7,11 +7,30 @@ import shoppingList from './shopping-list';
 
 import api from './api';
 
+import store from './store';
+
 const main = function () {
   api.getItems()
   .then(res => res.json())
-  .then(res => console.log(res));
-  console.log(api.BASE_URL);
+  .then((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  });
+
+  // api.createItem('pears')
+  // .then(res => res.json())
+  // .then((newItem) => {
+  //   return api.getItems();
+  // })
+  // .then(res => res.json())
+  // .then((items) => {
+  //   console.log(items);
+  // });
+
+  // api.getItems()
+  // .then(res => res.json())
+  // .then(res => console.log(res));
+  // console.log(api.BASE_URL);
  
   // fetch('https://thinkful-list-api.herokuapp.com/ei-student/items/victor-brian')
   // .then(res => res.json())
