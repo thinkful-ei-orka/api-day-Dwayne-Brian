4fb7902d794ca5e2eab955c025cfac19aa4d3d17
bcharles <bcharles41078@gmail.com>
2020-04-30 11:06:02 -0500

    3

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/';
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/ei-student/';
 
 function getItems() {
-return Promise.resolve('A successful response!');
+    return fetch(`${BASE_URL}items`)
+    //return Promise.resolve('A successful response!');
 }
 
 export default {
--- src/index.js
+++ src/index.js
@@ -9,12 +9,13 @@ import api from './api';
 
 const main = function () {
   api.getItems()
+  .then(res => res.json())
   .then(res => console.log(res));
   console.log(api.BASE_URL);
  
-  fetch('https://thinkful-list-api.herokuapp.com/ei-student/items/victor-brian')
-  .then(res => res.json())
-  .then(data => console.log(data));
+  // fetch('https://thinkful-list-api.herokuapp.com/ei-student/items/victor-brian')
+  // .then(res => res.json())
+  // .then(data => console.log(data));
   
   shoppingList.bindEventListeners();
   shoppingList.render();
