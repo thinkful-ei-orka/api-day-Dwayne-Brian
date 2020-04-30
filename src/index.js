7211350f52db7b413b5ef445ee8ebf5abe4ce94b
Victor Jarvis <vctrjrvs@gmail.com>
2020-04-30 11:35:56 -0400

    added api

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ import './index.css';
 
 import shoppingList from './shopping-list';
 
+import api from './api';
+
 const main = function () {
-  fetch('https://thinkful-list-api.herokuapp.com/ei-student/items')
+  api.getItems()
+  .then(res => console.log(res));
+  console.log(api.BASE_URL);
+ 
+  fetch('https://thinkful-list-api.herokuapp.com/ei-student/items/victor-brian')
   .then(res => res.json())
   .then(data => console.log(data));
   
